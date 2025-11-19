029b04053526863683ec1c931202952b29aedb08
Roy Warshavski <roywarshavski@Roys-MacBook-Pro.local>
2025-11-19 23:27:25 +0200

    version6

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -20,21 +20,24 @@ public class KeywordsDetector {
 
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
-    public static void detectAndPrint(String[] sentences, String[] keywords) {
+public static void detectAndPrint(String[] sentences, String[] keywords) {
 
     for (int i = 0; i < sentences.length; i++) {
-    for (int j = 0; j < keywords.length; j++) {
 
-       
-        String kw = MyString.lowerCase(keywords[j]);
+        String sentenceLower = MyString.lowerCase(sentences[i]);
 
-        if (MyString.contains(sentences[i], kw)) {
-            System.out.println(sentences[i]);
-            break;
+        for (int j = 0; j < keywords.length; j++) {
+
+            String kwLower = MyString.lowerCase(keywords[j]);
+
+            if (MyString.contains(sentenceLower, kwLower)) {
+                System.out.println(sentences[i]);
+                break;
+            }
         }
     }
 }
-}
+
 }
  
 
